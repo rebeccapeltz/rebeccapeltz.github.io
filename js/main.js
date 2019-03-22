5ec51b2117eac8480ec1bacb3401d3ac49302c83
rebeccapeltz <rebeccapeltz@gmail.com>
2019-03-21 17:22:57 -0700

    wire up switch

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let highlighter = undefined
 let counter = 0
 function highlightSkills() {
   let devIcons = document.querySelectorAll(".dev-icons .list-inline-item i")
-  highlighter = setInterval(function(){
+  return setInterval(function(){
     if (counter === devIcons.length) {
       counter = 0
     }
@@ -20,25 +20,35 @@ function highlightSkills() {
   },1000)
 }
 //toggle classes
-function switchClass(el, class1, class2){
-  if (el.hasClass(class1)){
-    el.removeClass(class1)
-    el.addClassList(class2)
+function switchClass(el, class1, class2, class1a, class2a){
+  if (el.classList.contains(class1)){
+    el.classList.remove(class1)
+    el.classList.remove(class1a)
+    el.classList.add(class2)
+    el.classList.add(class2a)
   } else {
-    el.removeClass(class2)
-    el.addClassList(class1)
+    el.classList.remove(class2)
+    el.classList.remove(class2a)
+    el.classList.add(class1)
+    el.classList.add(class1a)
   }
 
 }
 document.addEventListener("DOMContentLoaded",event =>{
-  // highlighter = highlightSkills()
+  highlighter = highlightSkills()
   let switchHighlighter = document.querySelector(".switch-highlighter")
  
   switchHighlighter.style.cursor = "hand" ///// ???
   switchHighlighter.addEventListener('click',event=>{
     console.log("highlighter switch clicked")
     // turn on intervals/turn off intervals
-    // switchClass(switchHighlighter,"turn-on","turn-off")
+    if (switchHighlighter.classList.contains("turn-off")){
+      clearInterval(highlighter)
+    } else {
+      highlighter = highlightSkills()
+    }
+    switchClass(switchHighlighter,"turn-on","turn-off", "fa-toggle-on", "fa-toggle-off")
+    
     // if (switchHighlighter.style.color === "red") switchHighlighter.style.color = "lime" // toggle 
     // else switchHighlighter.style.color = "red"
   })
